86d453579e942938a1df215267e83ad9a60354fe
khushboo1988 <khushboosatyarthi@gmail.com>
2026-05-04 16:02:24 +0530

    meaning full commit msg

---
+++ 03.py
@@ -0,0 +1,19 @@
+# Take string input
+string_value = input("Enter a string: ")
+
+# Take integer input
+int_value = int(input("Enter an integer: "))
+
+# Switch them
+string_value, int_value = int_value, string_value
+
+# Print the switched values
+print("Switched string:", string_value)
+print("Switched integer:", int_value)
+
+
+
+
+
+
+
---
+++ 04-new.py
@@ -0,0 +1,14 @@
+# switch one string value with one integer value input
+
+# n1 = 200,
+# char2 = "java"
+
+# output;
+
+n1 = "java"
+char2 =200
+temp=n1
+n1=char2
+char2=temp
+print("n1=",n1)
+print("char2=",char2)
