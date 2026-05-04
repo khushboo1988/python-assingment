64a2da4b629525493cae9fcd81de0b176bcd1dee
khushboo1988 <khushboosatyarthi@gmail.com>
2026-05-04 12:18:39 +0530

    new file added

---
+++ app.py
@@ -0,0 +1,19 @@
+# # Switch values of two integers Input:  n1 = 20, n2 = 30 Output:  n1 = 30 n2 = 20
+# n1 = 20
+# n2 = 30
+# tepm = n1
+# n1 = n2
+# n2 = tepm
+# print("n1=",n1)
+# print("n2=",n2)
+
+
+# 2 swith values of strings (characters)
+
+char1="hello" 
+char2="java"
+temp=char1 
+char1=char2
+char2=temp
+print("char1=",char1)
+print("char2=",char2)
