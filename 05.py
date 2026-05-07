3b9d750be8eca86e21acc8adb607a32c5d933091
khushboo1988 <khushboosatyarthi@gmail.com>
2026-05-07 16:31:16 +0530

    assiment

---
+++ 05.py
@@ -0,0 +1,40 @@
+#  #5 update balance after deposit
+ 
+# current_balance = 10000
+# deposit_amount = 5000
+# print("balance before deposit=" ,current_balance)
+# current_bprice=1000alance = current_balance + deposit_amount
+# print("balance after deposit=" ,current_balance)
+
+# # 6 update balance after withdrawal 
+# balance = 12000 
+# withdraw_amount = 3000 
+# print("balance before withdrawal=",balance)
+# balance = balance-withdraw_amount
+# print("balance after withdrawal=",balance)
+
+
+
+
+# # 7 increase shoping cart item by 3cart_items = 5
+# cart_items = 5
+# cart_items = cart_items + 3
+# print("total cart items =",cart_items)
+
+
+
+
+# 8 apply a 20% discount to a price
+
+# price=1000
+# discount = price*20/100
+# final_price = price - discount
+# print("final price =",final_price)
+
+
+
+# 9 calculate student persentage
+obtained_marks =430
+total_marks =500
+percentage = (obtained_marks/total_marks)*100
+print("percentage =", percentage)
