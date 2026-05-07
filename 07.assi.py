fdfb8928552410d86c06df9fda220e063808f0bd
khushboo1988 <khushboosatyarthi@gmail.com>
2026-05-08 00:17:35 +0530

    07.assi.py

---
+++ 07.assi.py
@@ -0,0 +1,78 @@
+# 21 calculate area of a tringle 
+
+# base=10 
+# height=6 
+# area=base*height*0.5 
+# print(f'input:base ={base}, height = { height}') 
+# print(f'output:area = {area}')
+
+# 22 calculate perimeter of a square 
+
+# side=9 
+# perimeter=side*4 
+# print(f'input:side={side}') 
+# print(f' output: perimeter = {perimeter}')
+
+# 23 calculate diameter of a circle 
+
+# radius=14 
+# diameter=radius*2 
+# print(f'input:radius = {radius}') 
+# print(f'output: diameter = {diameter}')
+
+# 24 calculate volume of a cube 
+
+# side=5 
+# volume=side**3 
+# print(f'input:side = {side}') 
+# print(f'output: volume = {volume}')
+
+# 25 calculate surface area of a cuboid 
+
+# l=4 
+# b=3 
+# h=2 
+# surface_area=(l*b+l*h+b*h)*2 
+# print(f'input: l={l}, b={b}, h={h}') 
+# print(f'output: surface area = {surface_area}')
+
+# 26 square pf sum:(x+y)^2 
+
+# x=5 
+# y=7 
+# res=(x+y)**2 
+# print(f'({x}+{y})^2={res}')
+
+# 27 simplyfy expression:x^2-4x+4 
+
+# x=3 
+# res=x**2-x*4+4 
+# print(f'{x}^2-4({x})+4={res}')
+
+# 25 evaluate: (a+b) (a-b) 
+
+# a=6 
+# b=2 
+# res=(a+b)*(a-b) 
+# print(f'({a}+{b})({a}-{b})={res}')
+
+# 29 sum of cubes: a^3 + b^3 
+
+# a=1 
+# b=2 
+# res=a**3+b**3 
+# print(f'{a}^3+{b}^3={res}')
+
+# 30 simplyfy: (x-y)^2 
+
+# x=10 
+# y=6 
+# res=(x-y)**2 
+# print(f'({x}-{y})^2={res}')
+
+# 31 difference of cube : x^3 - y^3 
+
+# x=4 
+# y=1 
+# res=x**3-y**3 
+# print(f'({x}^3-{y}^3)={res}')
