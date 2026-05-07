cb9a4aafcb1d8bfc063c4db22030d25fa0921828
khushboo1988 <khushboosatyarthi@gmail.com>
2026-05-08 00:07:29 +0530

    06.assi.py commit msg

---
+++ 06.assi.py
@@ -0,0 +1,90 @@
+# 10 calculate total and average of 4 subjects 
+# s1=80
+# s2=75
+# s3=90 
+# s4=85 
+# total = s1+s2+s3+s4 
+# average = total/4 
+# print("total=",total) 
+# print("average=",average)
+
+#11 calculate total and average of 3 numbers 
+
+# n1=25
+# n2=35
+# n3=45
+# average = (n1 + n2 + n3) /3
+# print("average=", average)
+
+# 12 PROFIT OR LOSS PERCENTAGE 
+
+# cost_price =500 
+# selling_price =600
+# profit=((selling_price-cost_price)/cost_price)*100
+# print(f'profit={profit}')
+
+# 13.calculate simple intrest 
+
+# principal = 10000 
+# rate = 5 
+# time = 2
+# simple_intrest=(principal*rate*time)/100 
+# print(f'simple intrest = {simple_intrest}')
+
+# 14 calculate compound intrest
+
+# principal = 10000
+# rate = 5
+# time = 2
+# compound_intrest = principal * (1 + rate / 100) ** time - principal
+# print(f'compound intrest = {compound_intrest}')
+
+# 15 calculate tax on income
+
+# income=50000 
+# tax_rate=10 
+# tax=income*tax_rate/100 
+# print(f'tax-{tax}')
+
+# 16 calculate percentage increase or decrease 
+
+# initial_value = 200 
+# final_value = 250 
+# percentage_change=((final_value-initial_value)*100)/initial_value 
+# print(f'percentage change={percentage_change}%')
+
+# 17 convert boolean to integer 
+
+# b1=True 
+# print(f'input:{b1}') 
+# b1=int(b1) 
+# print(f'output:{b1}')
+
+# 18 convert float to string 
+
+# num1=45.67 
+# print(f'input:{num1}') 
+# num1=str(num1) 
+# print(f'output:{num1}')
+
+# 19 convert 20 c to fahrenheit
+
+# temp_in_c=20 
+# temp_in_f=temp_in_c*9/5+32 
+# print(f'input:temp_in_c={temp_in_c}') 
+# print(f'output:temp_in_f={temp_in_f}')
+
+# 20 convert 50 f to celsius 
+
+# temp_in_f=50
+# temp_in_c=(temp_in_f-32)*5/9
+# print(f'input:temp_in_f={temp_in_f}')
+# print(f'output:temp_in_c={temp_in_c}')
+
+# 21 convert integer into binary 
+
+# num1= 25 
+# print(f'input:{num1}') 
+# bin1=f'{num1:b}' 
+# print(f'output:{bin1}')
+
